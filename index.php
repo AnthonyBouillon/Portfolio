119dbbac590a8dfb60464d1d17cdfac9e759ba3d
AnthonyBouillon <mastouche60@hotmail.fr>
2018-05-20 16:27:45 +0200

    Header du portfolio terminé

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="description de la page" />
+    <link href="https://fonts.googleapis.com/css?family=Bungee+Inline" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="Public/css/style.css">
+    <title>Portfolio d'un Développeur web junior</title>
+</head>
+<body>
+    <header>
+        <h1 class="text-center" id="title-h1">Dévelopeur web junior | Mes projets personnels</h1>
+    </header>
+    <!-- Menu -->
+    <nav class="navbar navbar-dark bg-dark sticky-top">
+        <a class="navbar-brand" href="http://www.anthony-bouillon.fr" title="Lien : CV numérique" target="_blank">Réalisé par : Anthony Bouillon</a>
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link" href="">RÉALISATION</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">CONTACT</a>
+            </li>
+        </ul>
+    </nav>
+    <!-- Corps de la page -->
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Première carte -->
+            <div class="col-lg-4">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <h2>Titre</h2>
+                    </div>
+                    <div class="card-body">
+                        <img src="https://fakeimg.pl/350x200/?text=Hello" alt="..." class="img-thumbnail">
+                    </div>
+                    <div class="card-footer text-muted">
+                        <div class="row">   
+                            <div class="col-xl-6">
+                                <button>Démonstration</button>
+                            </div>
+                            <div class="col-xl-6">
+                                <button>En savoir plus</button>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <!-- Deuxième carte -->
+            <div class="col-lg-4">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <h2>Titre</h2>
+                    </div>
+                    <div class="card-body">
+                        <img src="https://fakeimg.pl/350x200/?text=Hello" alt="..." class="img-thumbnail">
+                    </div>
+                    <div class="card-footer text-muted">
+                        <div class="row">   
+                            <div class="col-xl-6">
+                                <button>Démonstration</button>
+                            </div>
+                            <div class="col-xl-6">
+                                <button>En savoir plus</button>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            <!-- Troisième carte -->
+            <div class="col-lg-4">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <h2>Titre</h2>
+                    </div>
+                    <div class="card-body">
+                        <img src="https://fakeimg.pl/350x200/?text=Hello" alt="..." class="img-thumbnail">
+                    </div>
+                    <div class="card-footer text-muted">
+                        <div class="row">   
+                            <div class="col-xl-6">
+                                <button>Démonstration</button>
+                            </div>
+                            <div class="col-xl-6">
+                                <button>En savoir plus</button>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        <!-- / DIV ROW -->
+        </div>
+    <!-- / DIV CONTAINER-FLUID -->
+    </div>
+    <!-- Browser-sync, Bootstrap 4, myScript -->
+    <script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.24.4'><\/script>".replace("HOST", location.hostname));//]]></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="Public/js/script.js"></script>
+</body>
+</html>
